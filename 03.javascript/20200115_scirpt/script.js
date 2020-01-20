be16b6b8b1ddbd03b5f1aac87b17f73924265f38
99qkrtjdus <58929647+99qkrtjdus@users.noreply.github.com>
2020-01-20 12:03:46 +0900

    todayup

---
+++ 03.javascript/20200115_scirpt/script.js
@@ -0,0 +1,7 @@
+    	function han(obj){
+    		// alert("마우스오버금지");
+            obj.src = "../../img/banana.png";
+        }
+        function out(obj){
+            obj.src = "../../img/apple.png";    
+    	}
